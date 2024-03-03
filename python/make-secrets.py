dc6bbc14cff64abc0c3b638c568750219a76e48b
Calvin <calvin.house.jr@phoe-nix.xyz>
2024-03-03 21:38:31 +0000

    update make-secrets

--- python/make-secrets.py
+++ python/make-secrets.py
@@ -2,13 +2,19 @@ import secrets
 import string
 
 secret_len = int(input("Set Secret Length: "))
-secret_type = str.casefold(input("Use special characters (y/n)?: "))
 
-if secret_type == 'y':
-    secret_char = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(secret_char) for i in range(secret_len))
-elif secret_type == 'n':
-    secret_char = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(secret_char) for i in range(secret_len))
 
-print(password)
+if secret_len >= 8 and secret_len < 32:
+    secret_type = str.casefold(input("Use special characters (y/n)?: "))
+    if secret_type == 'y':
+        secret_char = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(secrets.choice(secret_char) for i in range(secret_len))
+        print(password)
+    elif secret_type == 'n':
+        secret_char = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(secret_char) for i in range(secret_len))
+        print(password)
+elif secret_len > 8:
+    print("Secret Length must be more than 8 characters.")
+elif secret_len < 32:
+    print("Secret Length must be less than 32 characters.")
