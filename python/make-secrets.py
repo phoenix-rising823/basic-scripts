331a762a27ca5daa280b9ec25dce2614e34431e7
phoenix-rising823 <calvin.house.jr@phoe-nix.xyz>
2024-04-19 22:29:14 -0400

    added some better control

--- python/make-secrets.py
+++ python/make-secrets.py
@@ -7,23 +7,30 @@ class SecreLengthError(Exception):
     pass
 
 
-try:
-    secret_len = int(input("Set Secret Length: "))
-    if secret_len < 8 or secret_len > 32:
-        raise SecreLengthError
-except SecreLengthError:
-    print("Secret Length must be between 8 and 32 characters...")
-except (TypeError, ValueError):
-    print("A valid value between 8 and 32 characters must be entered...")
-else:
-    secret_type = str.casefold(input("Use special characters (y/n)?: "))
-    if secret_type == 'y':
-        secret_char = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(secrets.choice(secret_char)
-                           for i in range(secret_len))
-        print(password)
-    elif secret_type == 'n':
-        secret_char = string.ascii_letters + string.digits
-        password = ''.join(secrets.choice(secret_char)
-                           for i in range(secret_len))
-        print(password)
+another_secret = "y"
+while another_secret.lower() == "y":
+    try:
+        secret_len = int(input("Set Secret Length: "))
+        if secret_len < 8 or secret_len > 32:
+            raise SecreLengthError
+    except SecreLengthError:
+        print("Secret Length must be between 8 and 32 characters...")
+        continue
+    except (TypeError, ValueError):
+        print("A valid value between 8 and 32 characters must be entered...")
+        continue
+    else:
+        secret_type = str.casefold(input("Use special characters (y/n)?: "))
+        if secret_type == 'y':
+            secret_char = (string.ascii_letters + string.digits +
+                           string.punctuation)
+            password = ''.join(secrets.choice(secret_char)
+                               for i in range(secret_len))
+            print(password)
+            another_secret = input("\nDo you want another secret? (y/n): ")
+        elif secret_type == 'n':
+            secret_char = string.ascii_letters + string.digits
+            password = ''.join(secrets.choice(secret_char)
+                               for i in range(secret_len))
+            print(password)
+            another_secret = input("\nDo you want another secret? (y/n): ")
