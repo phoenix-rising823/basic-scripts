f4484cc9e4ab120025916ef7dcf3f0b1d4b86f10
phoenix-rising823 <calvin.house.jr@phoe-nix.xyz>
2024-07-13 04:51:19 +0000

    changed to a command line tool

--- python/make-secrets.py
+++ python/make-secrets.py
@@ -1,36 +1,35 @@
 import secrets
 import string
+import argparse
 
 
-class SecreLengthError(Exception):
-    "Raised when Secret Length isn't between 8 and 32 characters"
-    pass
+def special_character_secret(secret_length=8):
+    sp_secret = ''.join(secrets.choice(string.ascii_letters + string.digits +
+                                       string.punctuation)
+                        for i in range(secret_length))
+    print(sp_secret)
 
 
-another_secret = "y"
-while another_secret.lower() == "y":
-    try:
-        secret_len = int(input("Set Secret Length: "))
-        if secret_len < 8 or secret_len > 32:
-            raise SecreLengthError
-    except SecreLengthError:
-        print("Secret Length must be between 8 and 32 characters...")
-        continue
-    except (TypeError, ValueError):
-        print("A valid value between 8 and 32 characters must be entered...")
-        continue
-    else:
-        secret_type = str.casefold(input("Use special characters (y/n)?: "))
-        if secret_type == 'y':
-            secret_char = (string.ascii_letters + string.digits +
-                           string.punctuation)
-            password = ''.join(secrets.choice(secret_char)
-                               for i in range(secret_len))
-            print(password)
-            another_secret = input("\nDo you want another secret? (y/n): ")
-        elif secret_type == 'n':
-            secret_char = string.ascii_letters + string.digits
-            password = ''.join(secrets.choice(secret_char)
-                               for i in range(secret_len))
-            print(password)
-            another_secret = input("\nDo you want another secret? (y/n): ")
+def alpha_num_secret(secret_length=8):
+    n_secret = ''.join(secrets.choice(string.ascii_letters + string.digits)
+                       for i in range(secret_length))
+    print(n_secret)
+
+
+parser = argparse.ArgumentParser(description="""Generate random secrets.
+                                  If no character length is provided,
+                                  the default is 8 characters.""")
+parser.add_argument("-s", "--special", action="store_const",
+                    const=special_character_secret,
+                    help="Secret with special characters.")
+parser.add_argument("-n", "--normal", action="store_const",
+                    const=alpha_num_secret,
+                    help="Alphanumeric secret.")
+parser.add_argument("-l", "--length", help="Set secret length", type=int)
+args = parser.parse_args()
+
+
+if args.special:
+    special_character_secret(args.length)
+elif args.normal:
+    alpha_num_secret(args.length)
